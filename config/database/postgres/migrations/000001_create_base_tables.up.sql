8ffbd9d44b0dcfbd73971688e49c46de6c1d471e
Micledson Carlos <micledson14@gmail.com>
2024-11-27 11:18:48 -0300

    set is_active by default true

--- config/database/postgres/migrations/000001_create_base_tables.up.sql
+++ config/database/postgres/migrations/000001_create_base_tables.up.sql
@@ -5,7 +5,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE todo (
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   description VARCHAR(30) NOT NULL,
-  is_active          BOOLEAN,
+  is_active          BOOLEAN default true,
   created_at         TIMESTAMP        DEFAULT NOW(),
   updated_at         TIMESTAMP        DEFAULT NOW()
 );
